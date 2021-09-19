c613f77b8f5b0b45c23c13ae63941370885f6d98
Noah <hanlun.song@gmail.com>
2021-09-18 22:43:00 -0400

    build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import reactJsx from 'vite-react-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), reactJsx()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '3d-bubble-background.js',
+        manualChunks: undefined,
+      },
+    },
+  },
 })
